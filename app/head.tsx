eb5b5f4bb32f69574bba4773205517ed2aae60c3
ikastner <ikastner@gitlab.com>
2025-05-20 15:02:11 +0200

    deploiement

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Planificateur de Vacances</title>
+      <link rel="icon" type="image/png" href="/favicon.png" />
+    </>
+  );
+} 
